65de497508f31a09400374338a867e0e9e659be3
Wolf Developer <lupo.andrea.93@gmail.com>
2021-03-15 08:32:46 +0100

    Dynamic component

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+import CartItem from "./CartItem.svelte";
+import Product from "./Product.svelte";
+
+let renderedComponent = {cmp: Product, title: 'Test Product', id: 'p1'};
+function toggle() {
+   if(renderedComponent.cmp ===  Product) {
+       renderedComponent = {cmp: CartItem, title: 'Another Product', id: 'p2'};
+   } 
+   else if(renderedComponent.cmp === CartItem) {
+       renderedComponent = {cmp: Product, title: 'Test Product', id: 'p1'};
+   }
+}
+</script>
+
+<button on:click={toggle}>Toggle display</button>
+
+
+<svelte:component this={renderedComponent.cmp} title={renderedComponent.title} id={renderedComponent.id}/>
+    
